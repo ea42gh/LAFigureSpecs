5eef74170555500405a388ffc406439d94e31db4
ea42gh <ea42_github@mail.com>
2026-05-23 13:52:32 -0400

    Update legacy GE alignment expectations

--- tests/test_ge_legacy_alignment.py
+++ tests/test_ge_legacy_alignment.py
@@ -12,7 +12,7 @@ def test_legacy_bg_uses_block_padding_for_right_align():
     assert codebefore
     # Column 0 has width 3 (from row 1), but row 2 col 0 is width 2.
     # With default block_align=auto (right), the leftmost entry is shifted by 1.
-    assert "(3-2)" in codebefore[0]
+    assert "fit = (3-|2) (4-|3)" in codebefore[0]
 
 
 def test_legacy_vertical_column_highlights_use_rule_corners():
@@ -22,7 +22,7 @@ def test_legacy_vertical_column_highlights_use_rule_corners():
     bg_list = [[0, 1, [((0, 1), (2, 1))], "yellow!40", 1]]
     codebefore = _legacy_bg_list_to_codebefore(matrices, bg_list)
     assert codebefore
-    assert "fit = (1-|2) (4-|3)" in codebefore[0]
+    assert "fit = (1-|5) (4-|6)" in codebefore[0]
 
 
 def test_legacy_ge_disables_format_nrhs_when_using_decorations(monkeypatch):
