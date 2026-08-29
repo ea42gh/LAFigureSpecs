cdf8538f07d82b869abd08eaec1bf4211d08b784
ea42gh <ea42_github@mail.com>
2026-08-29 10:21:25 -0400

    Promote LAFigureSpecs to 0.1.0

--- LAFigureSpecs/__about__.py
+++ LAFigureSpecs/__about__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2026-present ea42gh <ea42_github@mail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __build__ = "8e84e70 2026-02-06T17:43:30-05:00"
